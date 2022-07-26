6b26333dc77dfb043b5e97041437d66600698654
Thomas Marchand <thomas.marchand@tuta.io>
2022-07-26 19:16:27 +0200

    fix contract address

--- hooks/starknetId.js
+++ hooks/starknetId.js
@@ -4,6 +4,6 @@ import starknet_id_abi from "../abi/starknet_id_abi.json";
 export function useStarknetIdContract() {
   return useContract({
     abi: starknet_id_abi,
-    address: process.env.NEXT_PUBLIC_CONTRACT,
+    address: "0x04564121a7ad7757c425e4dac1a855998bf186303107d1c28edbf0de420e7023",
   });
 }
