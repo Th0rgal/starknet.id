2b28d3a28e095b11cf7c57aff7c2bd558d38593f
Thomas Marchand <thomas.marchand@tuta.io>
2022-08-05 13:42:25 +0200

    adapt to goerli

--- pages/discord.js
+++ pages/discord.js
@@ -64,7 +64,7 @@ export default function Discord() {
       }),
     };
 
-    fetch("https://verifier.starknet.id/sign", requestOptions)
+    fetch("https://goerli.verifier.starknet.id/sign", requestOptions)
       .then((response) => response.json())
       .then((data) => setSignRequestData(data));
   }, [code, tokenId]);
